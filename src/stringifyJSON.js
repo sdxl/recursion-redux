17553dac774920be880e18c75e98bd88164cddcb
MKS-students <mks.students@hackreactor.com>
2015-07-27 11:48:17 -0500

    Completed stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,52 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === 'number'){
+    return ''+ obj;
+  }
+
+  if (obj === null){
+    return 'null';
+  }
+
+  if (typeof obj === 'boolean'){
+    if (obj){
+      return "true";
+    }
+    else{
+      return "false";
+    }
+  }
+
+  if (typeof obj === 'string'){
+    return '"' + obj +'"';
+  }
+
+  if (Array.isArray(obj)){
+      var results = [];
+
+     for(var i=0; i<obj.length; i++){
+       results.push(stringifyJSON(obj[i]));
+     }
+
+     return "[" + results.join(',') + "]";
+  }
+
+  if (typeof obj === 'object'){
+    var results =[];
+
+    for(var key in obj){
+      if(typeof obj[key] === 'function' || obj[key] === undefined){
+        continue;
+      }
+
+      else{
+      results.push(stringifyJSON(key)+ ":" + stringifyJSON(obj[key]));
+      }
+
+    }
+
+    return "{" + results.join(',') + "}";
+  }
+
 };
