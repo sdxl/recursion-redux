8d1804bc18f0508611a776f11c46c6f0d253ae6b
MKS-students <mks.students@hackreactor.com>
2015-07-27 19:01:26 -0500

    Attempted parsonJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,37 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  if (json === 'true'){
+    return true;
+  }
+  if (json === 'false'){
+    return false;
+  }
+  if (json === 'undefined'){
+    return undefined;
+  }
+  if (json === 'null') {
+    return null;
+  }
+
+  if (json.slice(0,1) === "[") {
+    json = json.slice(1,json.length-1);
+    var arrayOfStrings = json.split(",");
+    var newArray = [];
+
+    for (var i = 0; i < arrayOfStrings.length; i++) {
+      newArray.push(parseJSON(arrayOfStrings[i]))`;
+    }
+
+    return newArray;
+
+  }
+
+  if (json.slice(0,1) === "{") {
+
+  }
+
+  return parseFloat(json);
+
+
 };
